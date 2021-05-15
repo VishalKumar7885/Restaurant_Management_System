c50853c10b5d67016efd0eb9135e7620ff9af957
VishalKumar7885 <84234112+VishalKumar7885@users.noreply.github.com>
2021-05-15 18:34:35 +0530

    Add files via upload

---
+++ restaurant/chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">A Festival Of Flavors</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>We are committed for uniting fresh, locally grown produce with farm-raised and wild-caught food to make the freshest dishes you’ll find anywhere. Our restaurant offers a variety of Indian crisp, Indian curry, and Fast Foods. Our values include providing fare wages for workers giving back to the communities we work with and serving great food!!</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/familypic.jfif" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
---
+++ restaurant/chunks/footer.php
@@ -0,0 +1,28 @@
+<section class="ffooter">
+		<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Us</h5>
+                <p class="grey-text text-lighten-4">Softora Road, Bengaluru</p>
+                <p class="grey-text text-lighten-4">Phone : +91 9870986578</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Social Media Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Whatsapp</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2021 Copyright @ Softora Restaurant
+            <a class="grey-text text-lighten-4 right" href="#!">Made by Vishal Kumar <span><i class="tiny material-icons">favorite</i></span></a>
+            </div>
+          </div>
+        </footer>
+	</section>
